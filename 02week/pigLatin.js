cfadb3f5b6728d508c40bc8b370406c6d459cc72
Amber Jones <amberlovescats14@gmail.com>
2019-02-27 16:21:13 -0600

    all vowels are functional

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -29,22 +29,62 @@ function pigLatin(word) {
      let together = apart.join('')
      return together
 
-   } 
+   } else if (indexE < adjusted.length && indexE > -1) {
+     
+    let adjustedCopy = adjusted.split()
+    let length = adjustedCopy.length
+    let vowelE=  indexE
+    let atVowel = adjusted.slice(vowelE)
+    let leftOver = adjusted.slice(0, vowelE)
+    let final = atVowel + leftOver
+    let apart = final.toString().split("")
+    let ending = apart.push('ay')
+    let together = apart.join('')
+    return together
 
+  }  else if (indexI < adjusted.length && indexI > -1) {
+     
+    let adjustedCopy = adjusted.split()
+    let length = adjustedCopy.length
+    let vowelI=  indexI
+    let atVowel = adjusted.slice(vowelI)
+    let leftOver = adjusted.slice(0, vowelI)
+    let final = atVowel + leftOver
+    let apart = final.toString().split("")
+    let ending = apart.push('ay')
+    let together = apart.join('')
+    return together
 
-  
-  
+  } else if (indexO < adjusted.length && indexO > -1) {
+     
+    let adjustedCopy = adjusted.split()
+    let length = adjustedCopy.length
+    let vowelO=  indexO
+    let atVowel = adjusted.slice(vowelO)
+    let leftOver = adjusted.slice(0, vowelO)
+    let final = atVowel + leftOver
+    let apart = final.toString().split("")
+    let ending = apart.push('ay')
+    let together = apart.join('')
+    return together
 
-  let letterA = "a"
-  let vowel1 = ['a', 'e', 'i', 'o', 'u']
-  // return wordString
-
-
-  // eachLetter.forEach(letter => {
-  //   if (wordString.indexOf() != vowel1){
-  //     return "true"
-  //   }
-  // });
+  } else {
+    if (indexU < adjusted.length && indexU > -1) {
+     
+      let adjustedCopy = adjusted.split()
+      let length = adjustedCopy.length
+      let vowelU=  indexU
+      let atVowel = adjusted.slice(vowelU)
+      let leftOver = adjusted.slice(0, vowelU)
+      let final = atVowel + leftOver
+      let apart = final.toString().split("")
+      let ending = apart.push('ay')
+      let together = apart.join('')
+      return together
+ 
+    } 
+ 
+  }
 
   
 }
