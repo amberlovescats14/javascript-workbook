ecfd5fde3f792865b0ac17388a6956267e89169a
Amber Jones <amberlovescats14@gmail.com>
2019-03-07 13:07:33 -0600

    starting script

---
+++ 02week/piglatin-gui/script.js
@@ -0,0 +1,8 @@
+var original = document.querySelector("#piglatin")
+var button = document.querySelector("#submit")
+
+function translate() {
+  let howToTranslate = 
+}
+
+button.addEventListener('click', translate)
