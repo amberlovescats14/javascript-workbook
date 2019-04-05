4531a5179a2d9dea239cb064264c129209e8dc6c
Amber Jones <amberlovescats14@gmail.com>
2019-04-04 20:45:10 -0500

    pulling from the api correctly

---
+++ 07week/pokemon/pok.js
@@ -0,0 +1,24 @@
+"use strict";
+function getPokemon() {
+  fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      let random = Math.floor(Math.random() * 20)
+      console.log(data.results[random].url)
+      return fetch(data.results[random].url)
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.species.name)
+      // return fetch()
+
+    })
+    .catch(function (error) {
+      console.log('Requestfailed', error)
+    });
+}
+let store = getPokemon()
